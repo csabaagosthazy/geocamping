886286719642b8f96508a8ce5f2d9e8797f32ebb
l4p1n <l4p1n@l4p1n.ch>
2022-06-17 11:49:13 +0200

    Add feature type and ID in the rentable objects serializer

--- geocamping/api/serializers.py
+++ geocamping/api/serializers.py
@@ -28,12 +28,20 @@ class FacilitySerializer(GeoFeatureModelSerializer):
 class BungalowSerializer(GeoFeatureModelSerializer):
     area = serializers.SerializerMethodField()
     price = serializers.SerializerMethodField()
+    feature_type = serializers.SerializerMethodField()
+    feature_id = serializers.SerializerMethodField()
 
     def get_area(self, obj):
         return round(obj.area.sq_m,1)
 
     def get_price(self, obj):
         return obj.price
+
+    def get_feature_type(self, obj):
+        return "bungalow"
+
+    def get_feature_id(self, obj):
+        return obj.id
 
     class Meta:
         model = Bungalow
@@ -43,12 +51,20 @@ class BungalowSerializer(GeoFeatureModelSerializer):
 class CottageSerializer(GeoFeatureModelSerializer):
     area = serializers.SerializerMethodField()
     price = serializers.SerializerMethodField()
+    feature_type = serializers.SerializerMethodField()
+    feature_id = serializers.SerializerMethodField()
 
     def get_area(self, obj):
         return round(obj.area.sq_m,1)
     
     def get_price(self, obj):
         return obj.price
+
+    def get_feature_type(self, obj):
+        return "cottage"
+
+    def get_feature_id(self, obj):
+        return obj.id
 
     class Meta:
         model = Cottage
@@ -60,24 +76,22 @@ class SlotSerializer(GeoFeatureModelSerializer):
 
     area = serializers.SerializerMethodField()
     price = serializers.SerializerMethodField()
+    feature_type = serializers.SerializerMethodField()
+    feature_id = serializers.SerializerMethodField()
 
     def get_area(self, obj):
         return round(obj.area.sq_m,1)
-    
 
     def get_price(self, obj):
         return obj.price
 
+    def get_feature_type(self, obj):
+        return "slot"
 
+    def get_feature_id(self, obj):
+        return obj.id
 
     class Meta:
         model = Slot
         geo_field="geom"
         fields = "__all__"
-
-
-    #area = serializers.SerializerMethodField()
-
-    #def get_area(self, obj):
-    #    obj.geom.transform(27700, clone=False)
-    #    return obj.geom.area
